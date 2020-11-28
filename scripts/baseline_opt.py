3b0cf0848211d8ece6ddd41b4188245f93e03d6b
sergio.gutierrez <sergio.gutierrez@sintec.com>
2020-11-28 04:36:32 -0500

    Creación de gatillo para ejecución de datamaster_base_opt.xlsx

---
+++ scripts/baseline_opt.py
@@ -0,0 +1,22 @@
+"""
+Código para ejecutar la optimización del baseline
+"""
+from output import *
+import time
+from limpieza_masters import limpieza_data
+
+
+start_time = time.time()
+sheet_names = ['master_producto', 'master_ubicaciones', 'master_demanda',
+               'master_tarifario', 'master_red_infraestructura']
+data = limpieza_data('input/datamaster_base_opt.xlsx', )
+
+output = [i for i in ejecucion(data)]
+output_names = ['base_opt_decision_consolidado.csv']
+output_path = 'output/'
+
+# Guardar output
+guardar_outputs([output[0]], [output_names[0]], output_path)
+print(f"Tiempo total de ejecucion: {time.time() - start_time} segundos\n",
+      f"Costo total operacion {output[2]} COP")
+# TODO: falta testear
