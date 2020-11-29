cf283eb8819d4a0604cf21f51456afca7e45056b
sergio.gutierrez <sergio.gutierrez@sintec.com>
2020-11-29 15:08:45 -0500

    Script listo y testeado

--- scripts/escenario.py
+++ scripts/escenario.py
@@ -9,7 +9,7 @@ from limpieza_masters import limpieza_data
 start_time = time.time()
 sheet_names = ['master_producto', 'master_ubicaciones', 'master_demanda',
                'master_tarifario', 'master_red_infraestructura']
-data = limpieza_data('input/datamaster_escenario.xlsx', )
+data = limpieza_data('input/datamaster_escenario.xlsx', sheet_names)
 
 output = [i for i in ejecucion(data)]
 output_names = ['escenario_decision_consolidado.csv']
@@ -19,4 +19,3 @@ output_path = 'output/'
 guardar_outputs([output[0]], [output_names[0]], output_path)
 print(f"Tiempo total de ejecucion: {time.time() - start_time} segundos\n",
       f"Costo total operacion {output[2]} COP")
-# TODO: falta testear
